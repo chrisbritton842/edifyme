b5027243263d4d9da6fdb866106a4d189646b992
Chris Britton <chrisbritton790@gmail.com>
2022-03-05 18:11:15 -0800

    started adding css for quora clone login form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -26,20 +26,22 @@ const LoginFormPage = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
-            <label>
-                Username or Email
+        <div className='login-input-fields'>
+            <form onSubmit={handleSubmit}>
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <label>
+                    Username or Email
                 <input type='text' value={credential} onChange={e => setCredential(e.target.value)} required />
-            </label>
-            <label>
-                Password
-                <input type='password' value={password} onChange={e => setPassword(e.target.value)} required />
-            </label>
-            <button type='submit'>Log In</button>
-        </form>
+                </label>
+                <label>
+                    Password
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} required />
+                </label>
+                <button type='submit'>Log In</button>
+            </form>
+        </div>
     );
 }
 
