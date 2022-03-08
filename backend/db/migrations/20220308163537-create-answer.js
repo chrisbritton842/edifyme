ca4da6b486e2e22bfbcebaa29966c9871562c208
Chris Britton <chrisbritton790@gmail.com>
2022-03-08 08:44:33 -0800

    set up migrations

---
+++ backend/db/migrations/20220308163537-create-answer.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Answers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      questionId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      answer: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Answers');
+  }
+};
