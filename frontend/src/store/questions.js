66e467ff9096d8df4ff4fb52640ec89aa5a65ef5
Chris Britton <chrisbritton790@gmail.com>
2022-03-10 14:45:51 -0800

    updated store to handle answers

--- frontend/src/store/questions.js
+++ frontend/src/store/questions.js
@@ -9,6 +9,50 @@ const setQuestions = (questions) => {
     };
 };
 
+export const createAnswer = ({ questionId, answer }) => async (dispatch) => {
+    await csrfFetch('/api/answers', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            questionId: questionId,
+            answer: answer
+        })
+    });
+
+    const allQuestions = await csrfFetch('/api/questions');
+    const data = await allQuestions.json();
+    dispatch(setQuestions(data));
+    return allQuestions;
+};
+
+export const updateAnswer = ({ answerId, newAnswer }) => async (dispatch) => {
+    await csrfFetch(`/api/answers/${answerId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            newAnswer: newAnswer
+        })
+    });
+
+    const allQuestions = await csrfFetch('/api/questions');
+    const data = await allQuestions.json();
+    dispatch(setQuestions(data));
+    return allQuestions;
+};
+
+export const deleteAnswer = (answerId) => async (dispatch) => {
+    await csrfFetch(`/api/answers/${answerId}`, { method: 'DELETE'})
+
+    const allQuestions = await csrfFetch('/api/questions');
+    const data = await allQuestions.json();
+    dispatch(setQuestions(data));
+    return allQuestions;
+}
+
 export const createQuestion = ({ newQuestion }) => async (dispatch) => {
     await csrfFetch('/api/questions', {
         method: 'POST',
